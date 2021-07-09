b3cac2ee32f95b789bb413cd0d5266e0a8d89dba
Ivan Chan <ichan018@ucr.edu>
2021-07-09 14:21:02 -0700

    Add exit, exit1 test files, change Makefile

---
+++ exit.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+//#include "stdio.h"
+
+int main(int argc, char *argv[]){
+    //hello();
+    printf(1,"running exit\n");
+    exit(0);
+//    return 0;
+}
---
+++ exit1.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+//#include "stdio.h"
+
+int main(int argc, char *argv[]){
+    //hello();
+    printf(1,"running exit1 (new exit call) \n");
+    exit1(0);
+//    return 0;
+}
