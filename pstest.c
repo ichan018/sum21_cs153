2d96d038485d8c6a53b1a4ccca9d592675bd89bd
Vipul Gupta <vipulguptawork16@gmail.com>
2021-08-06 10:37:19 -0700

    removed part of code where parent is highest priority, test case runs fine without it because main parent still runs

--- pstest.c
+++ pstest.c
@@ -14,13 +14,11 @@ main(void)
     printf(1, "Begin priority scheduling test\n");
     parentPid = getpid();
     printf(1, "Parent PID:%d\n", parentPid);
-    changeProcPriority(1); // make parent process high priority so it will keep running 
-                           // while first child priority is changed to high priority and is waiting
 
     pid1 = fork();
 
     if(pid1==0){
-      changeProcPriority(7); // set to higher priority than default, main program will keep running since it is higher
+      changeProcPriority(7); // set to higher priority than default, main program will keep running 
       sleep(1000);
       printf(1, "Exiting child process with priority 7: %d\n", getpid());
       exit();
@@ -29,12 +27,12 @@ main(void)
     pid2 = fork();
     
     if(pid2 == 0){
-      changeProcPriority(5);  // set to higher priority than default and first child, main program will keep running since it is higher
+      changeProcPriority(5);  // set to higher priority than default and first child, main program will keep running 
       sleep(1000);
       printf(1, "Exiting child process with priority 5: %d\n", getpid());
       exit();
     }
-    if(pid1>0 && pid2>0){ //main process waits for two children to finish before exiting
+    if(pid1>0 && pid2>0){ //main process waits for two children to finish before exiting (no zombie)
        printf(1, "Now wait in parent process %d for children to exit\n", getpid());
        wait();
        wait();
