a9cca3b1b7ed424c952c3bc31766fb52753ec26d
Vipul Gupta <vipulguptawork16@gmail.com>
2021-08-27 05:07:46 -0700

    modified page_fault_test.c so the test case works properly. Fixed minor error and now correct addresses are printed and the test case is more clear

--- page_fault_test.c
+++ page_fault_test.c
@@ -1,11 +1,13 @@
 //
 // This example program tries to call the system call page_fault() recursively to show the stack grows correctly.
 //
-// Usage: page_fault_test a0 a1 a2 a3 a4
-// Argument a0: The number of times you want to call system call page_fault()
-// Argument a1, a2, a3, a4: Can be any arbitary integer which we just pass to page_fault() as argument to put on the stack 
+// Usage: page_fault_test a b c d e
+// Argument a: The number of times you want to call function page_fault_test()
+// Argument b, c, d, e: Can be any arbitary integer which we just pass to page_fault_test() as argument to put on the stack 
 // 
-// Example: page_fault_test 1000 2 3 4 5 
+// Example: page_fault_test 1000 2 3 4 5      
+// Note: the above example should cause a page fault and our trap handler will account for this,
+// also the address of a0 will be printed out along with this and we can see where the stack stores it
 
 #include "types.h"
 #include "stat.h"
@@ -13,32 +15,30 @@
 int k;
 int m = 10;
 
-void page_fault_s(int a, int b, int c, int d, int e){
+void page_fault_test(int a, int b, int c, int d, int e){
     if(a == 0) {
-        printf(1, "level %d\n", a);
+        printf(1, "level %d address %p\n", a,  &a); //finish when a is 0
         exit();
     }else{
-        printf(1, "level %d address %p\n", a, &a);
-        
-        //execute system call page_fault() recursively
-        page_fault(a-1, b, c, d, e);
+        printf(1, "level %d address %p\n", a, &a); //print out address of a to show location on stack
+        //execute function page_fault_test() recursively
+        page_fault_test(a-1, b, c, d, e); 
     }
 }
 
 int
 main(int argc, char *argv[])
 {
-    int i = 0; // counter for for loop
 
     printf(1, "Begin page_fault_test test\n");
 
     k = atoi(argv[1]);
     if(k == 0) {
-        printf(1, "level %d\n", i);
-        printf(1, "Exiting code...\n", i);
+        printf(1, "level %d\n", k);
+        printf(1, "Exiting code... input argv[1] > 0 please!\n");
         exit();
     }else{
-        page_fault_s(k - 1, 2, 3, 4, 5);
+        page_fault_test(k - 1, 2, 3, 4, 5);
     }
 
     exit();
